5cc08dc1e7ef9263f49c9aeb4f53b9ace1ae9fc7
Jonathan.Whitehurst <m057326@gmail.com>
2022-11-13 21:33:06 -0500

    Homework Section 5 push

---
+++ 5_3-Homework.py
@@ -0,0 +1,8 @@
+#List containing Name, Age, Street Address, Vehicle Make, and Ownership of pet
+profile = ["Jonathan", 39, 2926, "Ford", True]
+
+profile[0]="Jim"
+
+profile.append = ["Cheesecake"]
+print(profile)
+
